6e879f06011adf6b326d5d7c3b6d9168e75a7f2f
Lorshan3550 <lorshannarayanasamy@gmail.com>
2023-10-10 21:05:03 +0530

    Do Level 1 Authentication

---
+++ app.js
@@ -0,0 +1,69 @@
+//jshint esversion:6
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+app.set("view engine", 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email : String,
+    password : String
+}
+
+const User = new mongoose.model("User", userSchema)
+
+
+app.get('/', function(req,res){
+    res.render('home')
+})
+
+app.get('/register' , function(req,res){
+    res.render('register')
+})
+
+app.get('/login', function(req,res){
+    res.render('login')
+})
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    })
+
+    newUser.save()
+    .then(res.render("secrets"))
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post("/login", function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email : username})
+    .then((foundUser) => {
+        if(foundUser.password === password){
+            res.render("secrets")
+        }
+        else{
+            res.redirect("/")
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.listen(port , () => {
+    console.log(`Server running on port ${port}`)
+})
